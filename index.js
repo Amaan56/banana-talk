9109b9b59345e617c0bf8b70f414e3d8a728edcb
Amaan56 <shaikhamaan257@gmail.com>
2020-12-06 15:06:06 +0530

    feat : translation logic added using fetch api

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Test');
+const inputArea = document.querySelector('.input-area');
+const outputArea = document.querySelector('.output-area');
+const translateBtn = document.querySelector('.translate-button');
+const baseURL = 'http://api.funtranslations.com/translate/minion.json';
+
+function constructUrl(searchParam) {
+  return `${baseURL}?text=${searchParam}`;
+}
+
+function translateInput() {
+  fetch(constructUrl(inputArea.value))
+    .then((response) => response.json())
+    .then((result) => {
+      outputArea.innerText = result.contents.translated;
+    })
+    .catch((err) => {
+      console.log(err);
+      alert('Something wrong with the request, Please try again');
+    });
+}
+
+translateBtn.addEventListener('click', translateInput);
